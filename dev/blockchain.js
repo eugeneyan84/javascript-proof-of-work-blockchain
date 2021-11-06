c5c052429f9372824e0f3da2c94a3def09e72261
eugene yan <eugene.yan@outlook.com>
2021-11-06 17:19:09 +0800

    hash and mining functions

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,10 @@
+const sha256 = require('sha256');
+
 function Blockchain() {
     this.chain = []; // all mined blocks will be stored in the chain
     this.pendingTxns = []; // to contain all new txns before placed into a block for mining
+
+    this.createNewBlock(100, '0', '0'); // genesis block creation
 };
 
 Blockchain.prototype.createNewBlock = function(nonce, previousHash, hash) {
@@ -33,6 +37,25 @@ Blockchain.prototype.createNewTxn = function(txnAmt, senderAddress, recipientAdd
     this.pendingTxns.push(newTxn); // append new transaction into existing transactions array
 
     return this.getLastBlock()['index'] + 1;
+};
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+    const dataStr = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hashValue = sha256(dataStr);
+
+    return hashValue;
+};
+
+Blockchain.prototype.mine = function(previousBlockHash, currentBlockData) {
+    let nonce = 0;
+    let hashValue = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while(hashValue.substring(0,4) !== '0000')
+    {
+        nonce += 1;
+        hashValue = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+
+    return nonce;
 };
 
 module.exports = Blockchain;
