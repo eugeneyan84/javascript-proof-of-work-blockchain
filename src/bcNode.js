3552ccdb65c5efa000dcf0335fadcc677bd6dcdb
eugene yan <eugene.yan@outlook.com>
2021-11-07 15:29:00 +0800

    request package added. 1st cut logic for node broadcast and registration across network.

---
+++ src/bcNode.js
@@ -0,0 +1,157 @@
+const express = require("express");
+const Blockchain = require("./blockchain");
+const uuid = require("uuid");
+const reqPromise = require("request-promise");
+const API_PORT = process.argv[2];
+
+console.log(process.argv[0]);
+console.log(process.argv[1]);
+console.log(process.argv[2]);
+
+const nodeAddress = uuid.v4().split("-").join("");
+console.log(`node-address: ${nodeAddress}`);
+var app = express();
+
+const bc = new Blockchain();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/blockchain", (req, res) => {
+  res.send(bc); // just return the whole chain
+});
+
+app.post("/transaction", (req, res) => {
+  const blockIndex = bc.createNewTxn(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+  res.json({ blockIndex: blockIndex });
+});
+
+app.get("/mine", (req, res) => {
+  // #1: get previous block
+  const previousBlock = bc.getLastBlock();
+  // #2: retrieve hash of previous block
+  const previousBlockHash = previousBlock["hash"];
+  // #3: construct new block with current set of pending transactions, plus a new index value (previous index + 1)
+  const newBlock = { txns: bc.pendingTxns, index: previousBlock["index"] + 1 };
+  // #4: brute force the new nonce out with the previous block's hash from #2, alongside the new block from #3
+  const computedNonce = bc.mine(previousBlockHash, newBlock);
+  console.log(`Computed nonce: ${computedNonce}`);
+  // #5: with the new nonce, derive the hash value of the combined previous block hash (#2), new block (#3) and new nonce (#4)
+  const newBlockHash = bc.hashBlock(previousBlockHash, newBlock, computedNonce);
+  console.log(`Computed new block hash: ${newBlockHash}`);
+  // #6: attach new block to the chain
+  const finalisedBlock = bc.createNewBlock(
+    computedNonce,
+    previousBlockHash,
+    newBlockHash
+  );
+  // #7: reward node for successfully mining new block
+  bc.createNewTxn(2, "00", nodeAddress);
+
+  res.json({
+    status: "New block successfully mined.",
+    resultBlock: finalisedBlock,
+  });
+});
+
+app.post("/register-and-broadcast", (req, res) => {
+  const newNodeUrl = req.body.newNodeUrl;
+  if (
+    bc.nodeNetwork.indexOf(newNodeUrl) == -1 &&
+    newNodeUrl != bc.currentNodeUrl
+  ) {
+    console.log(
+      `[register-and-broadcast] Registered new node url ${newNodeUrl}`
+    );
+    bc.nodeNetwork.push(newNodeUrl);
+  } else {
+    console.log(
+      `[register-and-broadcast] Node url ${newNodeUrl} already exists in network`
+    );
+  }
+  const promises = [];
+
+  // build requests that would be executed to every other existing node on the network
+  console.log(
+    `[register-and-broadcast] Building registration request(s) for the existing ${bc.nodeNetwork.length} node(s) in network`
+  );
+  bc.nodeNetwork.forEach((x) => {
+    const reqOptions = {
+      uri: x + "/register",
+      method: "POST",
+      body: { newNodeUrl: newNodeUrl },
+      json: true,
+    };
+    console.log(
+      `[register-and-broadcast] Current reqOptions: ${reqOptions.uri}, ${reqOptions.method}`
+    );
+    promises.push(reqPromise(reqOptions));
+  });
+
+  // execute the API call built from previous step, then build a bulk-registration request
+  console.log(`[register-and-broadcast] Executing registration broadcast`);
+  Promise.all(promises).then((resp) => {
+    console.log(resp);
+    console.log(
+      `[register-and-broadcast] Building bulk-registration request(s) for the existing ${bc.nodeNetwork.length} node(s) in network`
+    );
+    const bulkOptions = {
+      uri: newNodeUrl + "/bulk-register",
+      method: "POST",
+      body: {
+        allNetworkNodes: [...bc.nodeNetwork, bc.currentNodeUrl],
+      },
+      json: true,
+    };
+    console.log(`[register-and-broadcast] bulkOptions.body.allNetworkNodes: ${bulkOptions.body.allNetworkNodes.length}`);
+
+    // make bulk-registration API call to new node, then return a response
+    console.log(`[register-and-broadcast] Executing bulk-registration request`);
+    return reqPromise(bulkOptions).then((resp) => {
+      console.log(resp);
+      res.json({
+        sender: nodeAddress,
+        status: "New node registered with network successfully.",
+      });
+    });
+  });
+});
+
+app.post("/register", (req, res) => {
+  // register node with network
+  const newNodeUrl = req.body.newNodeUrl;
+  if (
+    bc.nodeNetwork.indexOf(newNodeUrl) == -1 &&
+    newNodeUrl !== bc.currentNodeUrl
+  ) {
+    bc.nodeNetwork.push(newNodeUrl);
+    console.log(`[register] Registered new node url ${newNodeUrl}`);
+  } else {
+    console.log(
+      `[register] Node url ${newNodeUrl} already exists in network`
+    );
+  }
+  res.json({ sender: nodeAddress, status: `New node (${newNodeUrl}) successfully registered` });
+});
+
+app.post("/bulk-register", (req, res) => {
+  const nodeUrls = req.body.allNetworkNodes;
+  nodeUrls.forEach((x) => {
+    console.log(`[bulk-register] Processing ${x}`)
+    if (bc.nodeNetwork.indexOf(x) == -1 && x !== bc.currentNodeUrl) {
+      bc.nodeNetwork.push(x);
+      console.log(`[bulk-register] Registered new node url ${x}`);
+    } else {
+      console.log(`[bulk-register] Node url ${x} already exists in network`);
+    }
+  });
+  res.json({ sender: nodeAddress, status: `Bulk registration successful` });
+});
+
+app.listen(API_PORT, () => {
+  console.log(`Listening on port ${API_PORT}...`);
+});
